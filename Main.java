64f4cb3ba1bee65b216b5786cf1a8b8cc9c0e74c
Margo-785 <mradich785@gmail.com>
2021-09-11 23:16:07 +0300

    Code.TXT

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 8850;
+        int mile = 20;
+        int bonus = price / mile;
+        System.out.println(bonus);
+    }
+}
